afe83767cc8458534f74cf73765eff8d407ca2dd
RalfBell <ralf.bell@nagarro.com>
2024-02-23 12:22:05 +0100

    Removed Discord, added Mastodon

--- bot.py
+++ bot.py
@@ -1,134 +1,45 @@
-# Birdini bot.py
-import os
-import discord
-import json
+import asyncio
 import datetime
 import requests
-
+import os
+from mastodon import Mastodon
+from birdbuddy.client import BirdBuddy
 from dotenv import load_dotenv
-from pybirdbuddy.birdbuddy.client import BirdBuddy
-from discord import app_commands
-from discord.ext import commands, tasks
-from discord.ext.commands import has_permissions
-from datetime import datetime
 
-# Load enviroment variables
+# Load environment variables or replace these with your actual credentials
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+ACCESS_TOKEN = os.getenv('MASTODON_ACCESS_TOKEN')
+URL = os.getenv('MASTODON_API_BASE_URL')
 EMAIL = os.getenv('BB_NAME')
 PASS = os.getenv('BB_PASS')
 
-# Open json file
-with open('discVars.json') as discVars:
-    jsonData = json.load(discVars)
+# Initialize Mastodon client
+mastodon = Mastodon(
+    access_token=ACCESS_TOKEN,
+    api_base_url=URL
+)
 
-# Login to birdbuddy feeder
+# Initialize BirdBuddy client
 bb = BirdBuddy(EMAIL, PASS)
-bb.language_code = "en"
+bb.language_code = "de"
 
-intents = discord.Intents.all()
+# Utility function to upload images to Mastodon and return media IDs
+def upload_images_to_mastodon(image_urls):
+    media_ids = []
+    for url in image_urls:
+        response = requests.get(url)
+        media = mastodon.media_post(response.content, "image/png")
+        media_ids.append(media['id'])
+    return media_ids
 
-# This is the URL used to insert multiple images into embeds. This can be changed but it's greatly appreciated if you leave it :)
-embedUrl = "https://ko-fi.com/denchy"
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-    bird_sighting_new.start()
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
-
-# Create buttons to attatch to embed
-
-
-class EmbedButtons(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-        # Always add support me button
-        supportMeButton = discord.ui.Button(
-            label="Support Me!", style=discord.ButtonStyle.blurple, url=embedUrl, emoji="❤️")
-        self.add_item(supportMeButton)
-
-# Change what channel the public messages will be sent too
-
-
-@bot.tree.command(name="channel", description="Define what channel you want to send public notifications to.")
-@has_permissions(administrator=True)
-@app_commands.describe(channel="Select a channel you wish to push notifications to. You can also enter the channel ID.")
-async def set_message_channel(interaction: discord.Interaction, channel: discord.TextChannel):
-    print(f"Message channel updated to {channel}")
-    jsonData['ChannelID'] = int(channel.id)
-    with open("discVars.json", "w") as fileA:
-        json.dump(jsonData, fileA)
-    await interaction.response.send_message(f"{bot.user.name} will now post notifications to {channel}", ephemeral=True)
-
-# Change what channel the 'muted' messages will be sent too
-
-
-@bot.tree.command(name="muted_channel", description="Define what channel you want to redirect public notifications to when muted.")
-@has_permissions(administrator=True)
-@app_commands.describe(channel="Select a channel you wish to redirect notifications to when muted. You can also enter the channel ID.")
-async def set_message_channel_muted(interaction: discord.Interaction, channel: discord.TextChannel):
-    print(f"Muted message channel updated to {channel}")
-    jsonData['mutedChannelID'] = int(channel.id)
-    with open("discVars.json", "w") as fileA:
-        json.dump(jsonData, fileA)
-    await interaction.response.send_message(f"{bot.user.name} will now post notifications to {channel} when muted", ephemeral=True)
-
-
-@bot.tree.command(name='mute', description="Mute the bot, the bot will continue to post notifications to your desired privated channel.")
-@has_permissions(administrator=True)
-async def mute_bot(interaction: discord.Interaction):
-    if jsonData['isMuted']:
-        await interaction.response.send_message(f"{bot.user.name} is already muted!", ephemeral=True)
-        return
-    jsonData['isMuted'] = True
-    with open("discVars.json", "w") as fileA:
-        json.dump(jsonData, fileA)
-    await interaction.response.send_message(f"{bot.user.name} is now muted and will no longer post to the public channel!", ephemeral=True)
-
-
-@bot.tree.command(name='unmute', description="Unmute the bot, let everyone see those beautiful birds again!")
-@has_permissions(administrator=True)
-async def unmute_bot(interaction: discord.Interaction):
-    if not jsonData['isMuted']:
-        await interaction.response.send_message(f"{bot.user.name} isn't muted!", ephemeral=True)
-        return
-    jsonData['isMuted'] = False
-    with open("discVars.json", "w") as fileA:
-        json.dump(jsonData, fileA)
-    await interaction.response.send_message(f"{bot.user.name} is now unmuted and will continue to post to the public channel!", ephemeral=True)
-
-
-# timer stuff
-@tasks.loop(seconds=60.0)
-async def bird_sighting_new():
-    channelID = jsonData['ChannelID']
-    mChannelID = jsonData['mutedChannelID']
-    isMuted = jsonData['isMuted']
-    # channel id you want the bird auto-notifications to be sent to
-    channel = bot.get_channel(channelID)
-    mChannel = bot.get_channel(mChannelID)
-    if channel is None:
-        print("A valid channel for public alerts was not selected.")
-        return
-    elif mChannel is None and isMuted is True:
-        print("A valid channel for muted alerts was not selected.")
-        return
-    # BB CODE
+# Example function to post a bird sighting on Mastodon
+async def post_bird_sighting():
     getPostcards = await bb.new_postcards()
 
     # return if no new postcards are detected
     if len(getPostcards) == 0:
         print("No new postcards found.")
         return
-
     # after postcard sighting is confirmed use finishPostcard
     getSightings = await bb.sighting_from_postcard(getPostcards[0])
     getReport = getSightings.report
@@ -167,52 +78,22 @@ async def bird_sighting_new():
             birdVisitCount = 1
             descriptionText = f"This is your first time being visited by a {birdName}!\n\n🐦 Total Visits: 1{appendToDescription}"
             embedTitle = f"{birdName} unlocked!"
-            embedColor = 0xf1c232
+            embedColor = 0xf1c232    
 
-    # Weird embed merge bug thing idk, allows for multiple images in a single embed. Mobile will only show one however.
-    embed = discord.Embed(title=embedTitle,
-                          url=embedUrl,
-                          description=descriptionText,
-                          color=embedColor,
-                          timestamp=datetime.now())
-    # embed.set_image(url=imageUrls[0])
-    image_data = requests.get(imageUrls[0]).content
-    with open('image.png', 'wb') as handler:
-        handler.write(image_data)
-    # ^old method linked expires, new one below for test... download and upload the files to discords server and then embed them
-    file = discord.File("image.png", filename="image.png")
-    files = [file]
+    # Upload images and get their media IDs
+    media_ids = upload_images_to_mastodon(imageUrls)
+    
+    # Construct the status text
+    status_text = f"#BirdBuddy reports {embedTitle} on {datetime.datetime.now().strftime('%d.%m.%Y %H:%M')}"
+    
+    # Post status with media on Mastodon
+    mastodon.status_post(status_text, media_ids=media_ids)
 
-    embed = discord.Embed(title=embedTitle,
-                          url=embedUrl,
-                          description=descriptionText,
-                          color=embedColor,
-                          timestamp=datetime.now())
-    embed.set_image(url="attachment://image.png")
-    embed.set_footer(text="Birdini by Denchy", icon_url=bot.user.avatar)
+# Async event loop to call post_bird_sighting periodically
+async def periodic_bird_sighting_posting(interval_seconds=3600):  
+    while True:
+        await post_bird_sighting()
+        await asyncio.sleep(interval_seconds)  # Wait for the specified interval before posting again
 
-    embeds = [embed]
-
-# Process additional images
-    if len(imageUrls) > 1:
-        for i in range(1, min(len(imageUrls), 4)):
-            image_data = requests.get(imageUrls[i]).content
-            with open(f'image{i}.png', 'wb') as handler:
-                handler.write(image_data)
-            file = discord.File(f"image{i}.png", filename=f"image{i}.png")
-            files.append(file)
-
-            embed = discord.Embed(url=embedUrl)
-            embed.set_image(url=f"attachment://image{i}.png")
-            embeds.append(embed)
-
-# Send embed with attachments to appropriate channel
-    if isMuted:
-        await mChannel.send(files=files, embeds=embeds, view=EmbedButtons())
-    else:
-        await channel.send(files=files, embeds=embeds, view=EmbedButtons())
-
-    # uncomment this to finish postcards and send them to collections!
-    # await bb.finish_postcard(getPostcards[0]['id'], getSightings)
-
-bot.run(TOKEN)
+# Run the event loop
+asyncio.run(periodic_bird_sighting_posting(60))  
